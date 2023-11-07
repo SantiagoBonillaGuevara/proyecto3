ccc26c18960e5a5fff8d689f576bea4e46b10ac9
Lufe04 <luisacarpintero01@gmail.com>
2023-11-06 20:48:02 -0500

    formulario arte completo

--- src/app/componentes/arte/arte.component.ts
+++ src/app/componentes/arte/arte.component.ts
@@ -1,17 +1,36 @@
 import { Component,OnInit,HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
+
 @Component({
   selector: 'app-arte',
   templateUrl: './arte.component.html',
   styleUrls: ['./arte.component.css']
 })
+
 export class ArteComponent implements OnInit {
-  ngOnInit(): void {
-    
-  }
+arteForm!:FormGroup;
+fechaActual: string;
+
+ngOnInit() {
+
+}
   @HostBinding('style.display') display = 'block';
-  constructor(private router: Router){
+  constructor(private fb: FormBuilder, private router: Router){
+    this.fechaActual = new Date().toISOString().split('T')[0];
+
+    this.arteForm = this.fb.group({
+      disciplina: ['', Validators.required],
+      estilo: ['', Validators.required],
+      experiencia: ['', Validators.required],
+      objetivo: ['', Validators.required],
+      materiales: ['', Validators.required],
+      proyectosEnGrupo: ['', Validators.required],
+      fecha: [this.fechaActual, Validators.required],
+      horario: ['', Validators.required]
+    });
   }
+
   irDatosPersonales(){
     this.display = 'none';
     this.router.navigate(['/datosPersonales'])
