3441963ec883f04b9b1607792fc2d8592f11cb72
Juanart15 <juanpoma@unisabana.edu.co>
2023-11-08 15:34:59 -0500

    forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './componentes/home/home.component';
 import { ArteComponent } from './componentes/arte/arte.component';
 import { MusicaComponent } from './componentes/musica/musica.component';
 import { DeporteComponent } from './componentes/deporte/deporte.component';
 import { DatosPersonalesComponent } from './componentes/datos-personales/datos-personales.component';
 import { DatosMedicosComponent } from './componentes/datos-medicos/datos-medicos.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path:'home', component: HomeComponent},
   {path:'arte', component: ArteComponent},
   {path:'musica', component: MusicaComponent},
   {path:'deporte', component: DeporteComponent},
   {path:'datosPersonales', component: DatosPersonalesComponent},
-  {path:'datosMedicos', component: DatosMedicosComponent}
+  {path:'datosMedicos', component: DatosMedicosComponent},
+  {path:'**',component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/componentes/musica/musica.component.ts
+++ src/app/componentes/musica/musica.component.ts
@@ -1,16 +1,29 @@
-import { Component,OnInit,HostBinding } from '@angular/core';
+import { Component,HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 @Component({
   selector: 'app-musica',
   templateUrl: './musica.component.html',
   styleUrls: ['./musica.component.css']
 })
 export class MusicaComponent {
-  ngOnInit(): void {
-    
-  }
+  deporteForm!:FormGroup;
+  fechaActual: string;
   @HostBinding('style.display') display = 'block';
-  constructor(private router: Router){
+  constructor(private fb: FormBuilder, private router: Router){
+    this.fechaActual = new Date().toISOString().split('T')[0];
+
+    this.deporteForm = this.fb.group({
+      clase: ['', Validators.required],
+      experiencia: ['', Validators.required],
+      modalidad: ['', Validators.required],
+      instrumentoPropio: ['', Validators.required],
+      generoPreferido: ['', Validators.required],
+      interes: ['', Validators.required],
+      dispuesto: ['', Validators.required],
+      gubernamentales: ['', Validators.required],
+      fecha: [this.fechaActual, Validators.required]
+    });
   }
   irDatosPersonales(){
     this.display = 'none';
